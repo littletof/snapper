20113f5e5a9ea7fc07e5e95b2c343a9f12ba1a8f
littletof <szalay.kristof.sopron@gmail.com>
2021-10-24 17:14:12 +0200

    Add types, verbose option, change default theme colors

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export { snap } from "./snapper.ts";
+export type { SnapParams, SnapOptions, ThemeColors} from "./snapper.ts";
 
 export {buildServer as buildSnapperServer, startServer as startSnapperServer} from "./server.ts";
--- snapper.ts
+++ snapper.ts
@@ -4,15 +4,47 @@ import { startServer } from "./server.ts";
 export interface SnapParams {
     imageSavePath: string;
     content: string;
-    viewport?: {width: number, height: number, deviceScaleFactor?: number},
+    viewport?: {width: number, height: number, deviceScaleFactor?: number}, // TODO get scaleFactor out, make w/h optional
+}
+
+export interface SnapOptions {
+    snapServerUrl?: string;
+    theme?: ThemeColors;
+    puppeteerLaunchOptions?: PuppeteerLaunchOptions;
+    verbose?: boolean;
+}
+
+export interface ThemeColors {
+    foreground?: string;
+    background?: string;
+    black?: string;
+    brightBlack?: string;
+    red?: string;
+    brightRed?: string;
+    green?: string;
+    brightGreen?: string;
+    yellow?: string;
+    brightYellow?: string;
+    blue?: string;
+    brightBlue?: string;
+    magenta?: string;
+    brightMagenta?: string;
+    cyan?: string;
+    brightCyan?: string;
+    white?: string;
+    brightWhite?: string;
 }
 
 // TODO Deno puppeteer types
 export type PuppeteerLaunchOptions = any;
 
-export async function snap(snaps: SnapParams[], options?: {snapServerUrl?: string, theme?: any, puppeteerLaunchOptions?: PuppeteerLaunchOptions}) {
+export async function snap(snaps: SnapParams[], options?: SnapOptions) {
+    const log = createLog(options);
+    log('\x1b[42m \x1b[1m\x1b[37mSnapper\x1b[39m\x1b[22m 📷  \x1b[49m');
+    log('\nStarting server...');
     const {listen, abort} = startServer();
 
+    log('Launching puppeteer...');
     const browser = await puppeteer.launch(options?.puppeteerLaunchOptions || {
         env: { PUPPETEER_PRODUCT: 'chrome' },
         defaultViewport: {width: 1080, height: 30},
@@ -21,7 +53,7 @@ export async function snap(snaps: SnapParams[], options?: {snapServerUrl?: strin
     });
 
     const page = await browser.newPage();
-    
+    log('\nSnapping pictures: ');
     for(let textCase of snaps) {
         const viewPort = {deviceScaleFactor: 3, width: 1080, height: 30, ...textCase.viewport};
         // TODO handle headless looking different
@@ -55,6 +87,7 @@ export async function snap(snaps: SnapParams[], options?: {snapServerUrl?: strin
         await page.goto(options?.snapServerUrl || `http://localhost:7777`);
         await page.waitForSelector('#done');
         // TODO checkexists imageSavePath
+        log(`\t- ${textCase.imageSavePath}`);
         await page.screenshot({ path: textCase.imageSavePath, fullPage: !textCase.viewport });
     }
 
@@ -62,5 +95,13 @@ export async function snap(snaps: SnapParams[], options?: {snapServerUrl?: strin
 
     abort();
     await listen;
+    log('\nPuppeteer, server closed.\n\x1b[42m \x1b[1m\x1b[37mSnapping done \x1b[39m\x1b[22m\x1b[49m');
+}
+
+export function createLog(options?: SnapOptions) {
+    if(options?.verbose) {
+        return (...text: any[]) => console.log(...text);
+    }
+    return (...text: any[]) => {}
 }
 
