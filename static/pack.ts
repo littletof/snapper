eae2ab53243215f66c2ebbb66a0377bd81df3580
littletof <szalay.kristof.sopron@gmail.com>
2022-07-30 16:47:30 +0200

    chore: Update XtermJS to 4.19.0, add deno.json, update doc images

--- static/pack.ts
+++ static/pack.ts
@@ -1,6 +1,7 @@
 const data: any = {};
 
 data['fitAddon.js'] = Deno.readTextFileSync('static/xterm/fitAddon.js');
+// https://cdn.jsdelivr.net/npm/xterm@4.19.0/lib/xterm.min.js
 data['xterm.js'] = Deno.readTextFileSync('static/xterm/xterm.js');
 data['xterm.css'] = Deno.readTextFileSync('static/xterm/xterm.css');
 
